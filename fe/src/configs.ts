dedb7f5c889f032c188808875bf6abb4ee68bc53
Luu Truong Sinh <sinhlt58@gmail.com>
2022-12-27 23:17:55 +0700

    update

--- fe/src/configs.ts
+++ fe/src/configs.ts
@@ -2,7 +2,7 @@ export const ENV_DEVELOPMENT = "development";
 export const ENV_PRODUCTION = "production";
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+  process.env.REACT_APP_API_BASE_URL || "";
 export const ENV = process.env.REACT_APP_ENV || ENV_DEVELOPMENT;
 
 export const configs = {
